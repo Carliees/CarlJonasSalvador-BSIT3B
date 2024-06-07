24971db3ec456e4c3d87797269544469d1e78826
unknown <ceciliacabanding2@gmail.com>
2024-06-06 22:12:32 -0600

    newnew

--- app/Http/Controllers/employmngcontroler.php
+++ app/Http/Controllers/employmngcontroler.php
@@ -16,7 +16,10 @@ class employmngcontroler extends Controller
         $response['employee_sample'] = $this->uniqueemployee->all();
         return view('employee-mngt.index')->with($response);
     }
-    public function create(){
+    public function table(){
+        $response['employee_sample'] = $this->uniqueemployee->all();
+        return view('employee-mngt.index')->with($response);
+    public function create()}{
       
     }
     public function store(Request $request){
